de6a002c96b9774c32cbd98c486219cdaa4451da
Lin Jack <linjack@192.168.0.31>
2023-12-20 15:09:18 +0800

    Less search

--- GraphDataComplete.py
+++ GraphDataComplete.py
@@ -52,13 +52,27 @@ with open('data.txt', 'r') as file:
 
 # skip the first line which is the header
 lines = lines[1:]
+features_list = []
+# loop through each line in the file, save all the unique column 8, "feature"
+for line in lines:
+    columns = line.split('\t')
+    feature = columns[8]
+    if feature not in features_list:
+        features_list.append(feature)
+print(features_list)
+
+mane_select_list = []
+# check mane select for each feature
+# save the MANE Select information
+for feature in features_list:
+    if check_mane_select(feature):
+        mane_select_list.append(feature)
 
 # Loop through each line in the file and check for 'MANE Select'
 for line in lines:
     columns = line.split('\t')
     feature = columns[8]
-    print(feature)
-    if check_mane_select(feature):
+    if feature in mane_select_list:
         #print(line)  # or save the information as needed
         #save the infromation to a new file
         with open('MANE_select.txt', 'a') as file:
--- MANE_SELECTION.py
+++ MANE_SELECTION.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 # set up the browser
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+# Method for checking if a feature is MANE Select
 def check_mane_select(feature):
     url = f"https://www.ncbi.nlm.nih.gov/nuccore/{feature}"
     driver.get(url)
@@ -18,6 +19,7 @@ def check_mane_select(feature):
     except:
         return False
     return False
+
 #clear previous data
 with open('MANE_select.txt', 'w') as file:
     file.write('')
@@ -29,13 +31,28 @@ with open('data.txt', 'r') as file:
 
 # skip the first line which is the header
 lines = lines[1:]
+features_list = []
+
+# loop through each line in the file, save all the unique column 8, "feature"
+for line in lines:
+    columns = line.split('\t')
+    feature = columns[8]
+    if feature not in features_list:
+        features_list.append(feature)
+print(features_list)
+
+mane_select_list = []
+# check mane select for each feature
+# save the MANE Select information
+for feature in features_list:
+    if check_mane_select(feature):
+        mane_select_list.append(feature)
 
 # Loop through each line in the file and check for 'MANE Select'
 for line in lines:
     columns = line.split('\t')
     feature = columns[8]
-    print(feature)
-    if check_mane_select(feature):
+    if feature in mane_select_list:
         #print(line)  # or save the information as needed
         #save the infromation to a new file
         with open('MANE_select.txt', 'a') as file:
